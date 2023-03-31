f2d277e14c91016cecc2251288df41b5e45d77ec
Adriano <adriano_alves_2008@hotmail.com>
2023-03-31 20:09:14 -0300

    Exibindo issues

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -51,6 +51,24 @@ export default function Repositorio() {
         <h1>{repo.name}</h1>
         <p>{repo.description}</p>
       </C.Owner>
+
+      <C.IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a hrfe={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </C.IssuesList>
     </C.Container>
   )
 }
