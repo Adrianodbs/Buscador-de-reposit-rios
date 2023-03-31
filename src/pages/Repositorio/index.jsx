e17cb024a9ecda372d2b875042c6e624db707354
Adriano <adriano_alves_2008@hotmail.com>
2023-03-31 14:05:34 -0300

    Carregando detalhes

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,12 +1,33 @@
 import { useParams } from 'react-router-dom'
+import * as C from './styles'
+import { useState, useEffect } from 'react'
+import api from '../../services/api'
 
 export default function Repositorio() {
-  const { repositorio } = useParams()
+  const { repo } = useParams()
 
-  return (
-    <div>
-      <h1>Repositorio</h1>
-      <span>{repositorio}</span>
-    </div>
-  )
+  const [repositorio, setRepositorio] = useState({})
+  const [issues, setIssues] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    async function load() {
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${repo}`),
+        api.get(`/repos/${repo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ])
+
+      setRepositorio(repositorioData.data)
+      setIssues(issuesData.data)
+      setLoading(false)
+    }
+
+    load()
+  }, [repo])
+
+  return <C.Container></C.Container>
 }
