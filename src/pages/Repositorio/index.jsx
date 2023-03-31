dfe5c5a4278622db5e89f5269f7868b40b58f352
Adriano <adriano_alves_2008@hotmail.com>
2023-03-31 19:48:51 -0300

    exibindo detalhes

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -3,17 +3,19 @@ import * as C from './styles'
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
 
-export default function Repositorio() {
-  const { repo } = useParams()
+import { FaArrowLeft } from 'react-icons/fa'
 
-  const [repositorio, setRepositorio] = useState({})
+export default function Repositorio() {
+  const { repositorio } = useParams()
+
+  const [repo, setRepo] = useState({})
   const [issues, setIssues] = useState([])
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     async function load() {
       const [repositorioData, issuesData] = await Promise.all([
-        api.get(`/repos/${repo}`),
-        api.get(`/repos/${repo}/issues`, {
+        api.get(`/repos/${repositorio}`),
+        api.get(`/repos/${repositorio}/issues`, {
           params: {
             state: 'open',
             per_page: 5
@@ -21,13 +23,34 @@ export default function Repositorio() {
         })
       ])
 
-      setRepositorio(repositorioData.data)
+      console.log(repositorioData.data)
+
+      setRepo(repositorioData.data)
       setIssues(issuesData.data)
       setLoading(false)
     }
 
     load()
-  }, [repo])
+  }, [repositorio])
 
-  return <C.Container></C.Container>
+  if (loading) {
+    return (
+      <C.Loading>
+        <h1>Carregando</h1>
+      </C.Loading>
+    )
+  }
+
+  return (
+    <C.Container>
+      <C.BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </C.BackButton>
+      <C.Owner>
+        <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+        <h1>{repo.name}</h1>
+        <p>{repo.description}</p>
+      </C.Owner>
+    </C.Container>
+  )
 }
